44b7be86b1e7f0126042699cab3989002eb69401
reza kheradmandi <kheradmandi.partotech@gmail.com>
2024-12-01 14:34:06 +0330

    the base url is changed

--- helper/axiosInstance.tsx
+++ helper/axiosInstance.tsx
@@ -20,7 +20,7 @@ interface ServerSessionRequest {
 }
 
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
     },
